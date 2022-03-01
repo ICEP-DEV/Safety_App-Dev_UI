bf7fe949b1f644de345da32cc75c69602fedae33
ofentse@121 <ofentsemolepolle121@gmail.com>
2022-03-01 09:38:05 +0200

    merged

---
+++ lib/requestchart.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RequestChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Request Chat"),
+        centerTitle: true,
+        backgroundColor: Colors.red[700],
+      ),
+    );
+  }
+}
