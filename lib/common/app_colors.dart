7b8e418a5fac8e942c57632e29239b37d0b2398e
unknown <giftmangena222@gmail.com>
2022-05-20 09:01:45 +0200

    admin

---
+++ lib/common/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color red = Color.fromARGB(255, 218, 24, 10);
+  static Color black = Colors.black;
+  static Color white = Colors.white;
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:admin/controller/menu_controller.dart';
+import 'package:admin/pages/home_page.dart';
+import 'package:admin/pages/locator.dart';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => MenuController()),
+        ],
+        child: HomePage(),
+      ),
+    );
+  }
+}
